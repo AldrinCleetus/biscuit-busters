8de35be5b055d4dd7ebf20e1c5eda4da89696103
Aldrin <cleetusaldrin30@gmail.com>
2025-08-30 12:59:07 +0530

    Added basic character movement

---
+++ scripts/player_main.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 120
+
+
+func _physics_process(_delta: float) -> void:
+
+	var direction = Vector2(
+   	 Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+   	 Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+
+	direction = direction.normalized()
+
+	velocity = direction * SPEED
+
+	move_and_slide()
